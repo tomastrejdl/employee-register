c047bbb8701d3717ae061cdfad396c5c9f9154fb
Tomáš Trejdl <trejdtom@fit.cvut.cz>
2017-10-31 17:47:37 +0100

    Doctine database 2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Trejdl\EmployeeBundle\TrejdlEmployeeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
